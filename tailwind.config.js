7e26d7199592530eaaa0098bae0e41e0db901be5
Talha <zubaer.talha20@gmail.comm>
2023-07-27 00:42:12 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        lightRed: '#F85559',
+        lightGray: '#121212',
+      },
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
